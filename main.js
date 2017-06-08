afca5c220bd3c6d047312e3f9ebeb8fa917a031c
joeylaguna <elagunaj@gmail.com>
2017-06-08 11:28:21 -0700

    Complete logic for talent calc.  Need to refactor into functions

--- main.js
+++ main.js
@@ -8,22 +8,30 @@ window.onload = () => {
     let button = e.button;
     //left click - add talent
     if (button === 0) {
-    let talentActive = e.target.attributes[2].value;
-    let talentIndex = e.target.attributes[3].value;
-
+      let talentActive = e.target.attributes[2].value;
+      let talentIndex = e.target.attributes[3].value;
+      
       if (numberOfTalentPoints < 6) {
         if (talentActive === 'false') {
-          numberOfTalentPoints++;
-          talentPoints.innerHTML = numberOfTalentPoints;
-          e.target.attributes[2].value = true;
-          e.target.style.background = 'url(assets/talent-icons-sprite.png) no-repeat 0% 100.545%';
+          if (e.target.attributes[0].value === 'talentOne' || e.target.attributes[0].value === 'talentFive') {
+            numberOfTalentPoints++;
+            talentPoints.innerHTML = numberOfTalentPoints;
+            e.target.attributes[2].value = true;
+            e.target.style.background = 'url(assets/talent-icons-sprite.png) no-repeat 0% 100.545%';
+          } else {
+              let previousTalent = e.target.previousElementSibling.previousElementSibling;
+              if (previousTalent.attributes[2].value === 'true') {
+                numberOfTalentPoints++;
+                talentPoints.innerHTML = numberOfTalentPoints;
+                e.target.attributes[2].value = true;
+                e.target.style.background = 'url(assets/talent-icons-sprite.png) no-repeat 0% 100.545%';
+              }
+          } 
         }
       }
-      
-      
     } else {
-      //left click - remove talent
-      if (e.target.attributes[2].value === 'true') {
+      //right click - remove talent
+        if (e.target.attributes[2].value === 'true') {
         numberOfTalentPoints--;
         talentPoints.innerHTML = numberOfTalentPoints
         e.target.attributes[2].value = false;
@@ -44,7 +52,7 @@ window.onload = () => {
   });
 
   talentTrees[1].childNodes.forEach((value) => {
-    if (value.attributes) {
+    if (value.className !== 'path') {
       let self = this;
       value.addEventListener('click', handler, false);
       value.addEventListener('contextmenu', handler, false);
